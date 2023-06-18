15af69798fec2f00b43ef9d2a02423b53e9065ce
Martijn van Exel <m@rtijn.org>
2023-06-18 11:40:25 -0600

    improve shell script

--- osm-vintage-tile-server.sh
+++ osm-vintage-tile-server.sh
@@ -40,13 +40,74 @@ else
     esac
 
     echo "OK"
-    echo "Slicing planet for Jan 1 ${YEAR}..."
-    osmium time-filter --progress -o ${YEAR}.osm.pbf ${OSM_FILE} ${YEAR}-01-01T00:00:00Z 
+
+    # skip time-filter step if ${YEAR}.osm.pbf already exists
+    if ! [[ -f ${YEAR}.osm.pbf ]]; then
+        echo "Slicing planet for Jan 1 ${YEAR}..."
+        osmium time-filter --progress -o ${YEAR}.osm.pbf ${OSM_FILE} ${YEAR}-01-01T00:00:00Z 
+    else
+        echo "Skipping time-filter step. ${YEAR}.osm.pbf already exists."
+    fi
+    
+    # quietly check the integrity of the PBF file, and exit if it's not OK
+    echo "Checking integrity of ${YEAR}.osm.pbf..."
+    osmium fileinfo --no-progress ${YEAR}.osm.pbf >/dev/null 2>&1 || { echo >&2 "File integrity check failed. Please try again."; exit 1; } 
+    
+    # stop and delete any existing Docker containers
+    if docker ps -a | grep -q osm-import-${YEAR}; then
+        echo "Stopping and deleting Docker container osm-import-${YEAR}..."
+        docker stop osm-import-${YEAR} >/dev/null 2>&1
+        docker rm osm-import-${YEAR} >/dev/null 2>&1
+    fi
+    if docker ps -a | grep -q osm-vintage-tile-server-${YEAR}; then
+        echo "Stopping and deleting Docker container osm-vintage-tile-server-${YEAR}..."
+        docker stop osm-vintage-tile-server-${YEAR} >/dev/null 2>&1
+        docker rm osm-vintage-tile-server-${YEAR} >/dev/null 2>&1
+    fi
+    
+    # delete any existing tile server and import images
+    if docker images | grep -q overv/openstreetmap-tile-server; then
+        echo "Deleting Docker image overv/openstreetmap-tile-server..."
+        docker rmi overv/openstreetmap-tile-server >/dev/null 2>&1
+    fi
+    if docker images | grep -q overv/openstreetmap-tile-server-import; then
+        echo "Deleting Docker image overv/openstreetmap-tile-server-import..."
+        docker rmi overv/openstreetmap-tile-server-import >/dev/null 2>&1
+    fi
+
+    # check if the docker volumes exist, if they do, ask the user if they want to delete them
+    if docker volume ls | grep -q openstreetmap-data-${YEAR}; then
+        echo "Docker volume openstreetmap-data-${YEAR} already exists."
+        read -p "Do you want to delete it? (y/n) " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            echo "Deleting Docker volume openstreetmap-data-${YEAR}..."
+            docker volume rm openstreetmap-data-${YEAR}
+        fi
+    fi
+    if docker volume ls | grep -q openstreetmap-tiles-${YEAR}; then
+        echo "Docker volume openstreetmap-tiles-${YEAR} already exists."
+        read -p "Do you want to delete it? (y/n) " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            echo "Deleting Docker volume openstreetmap-tiles-${YEAR}..."
+            docker volume rm openstreetmap-tiles-${YEAR}
+        fi
+    fi
+
+    # build the Docker image
     echo "Creating Docker volumes..."
     docker volume create openstreetmap-data-${YEAR}
     docker volume create openstreetmap-tiles-${YEAR}
+    
+    # run the data import
     echo "Creating Docker container and importing ${YEAR} data. This will take minutes to hours..."
     docker run -v ${BASEDIR}/${YEAR}.osm.pbf:/data/region.osm.pbf -v openstreetmap-data-${YEAR}:/data/database/ -v openstreetmap-tiles-${YEAR}:/data/tiles/ --name osm-import-${YEAR} overv/openstreetmap-tile-server import >/dev/null 2>&1
+    docker logs -f osm-import-${YEAR} >/dev/null 2>&1 &
+
+    # start the tile server container
     echo "Starting Container..."
     docker run -p ${PORT}:80 -v openstreetmap-data-${YEAR}:/data/database/ -v openstreetmap-tiles-${YEAR}:/data/tiles/ --name osm-vintage-tile-server-${YEAR} -d overv/openstreetmap-tile-server run
     echo "Done. Please visit http://localhost:${PORT} to enjoy your vintage tiles."
